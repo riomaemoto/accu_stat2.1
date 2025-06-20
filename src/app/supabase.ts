9a70c1e93779f380c4e753e78f7ae637f1dcd058
rio maemoto <leo.a1025ark@gmail.com>
2025-06-20 13:26:19 +0900

    Remove unused

--- src/app/supabase.ts
+++ src/app/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
 import { StatType } from "./atom";
-import { error } from "console";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
